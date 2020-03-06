a71cb50adc036953b04c8b5a7cfb5c1874711509
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-03-06 09:34:04 +0000

    external css link

--- src/js/main.js
+++ src/js/main.js
@@ -6,15 +6,40 @@ document.body.appendChild(el);
 document.body.classList.add('header-atom');
 
 let parentDoc = window.parent.document;
-let styleAll = document.querySelectorAll('style');
-let newStyle = styleAll[(styleAll.length - 1)].cloneNode(true);
-parentDoc.body.appendChild(newStyle);
+// let styleAll = document.querySelectorAll('style');
+// let newStyle = styleAll[(styleAll.length - 1)].cloneNode(true);
+// parentDoc.body.appendChild(newStyle);
 
-console.log('1744');
+parentDoc.body.dataset.deploy = '930';
 
-// let h = parentDoc.querySelector('.immersive-main-media__headline-container');
-// h.remove();
+let newStyleLink = document.createElement('link');
+newStyleLink.setAttribute('rel', 'stylesheet');
+newStyleLink.setAttribute('href', 'https://uploads.guim.co.uk/2020/03/06/main.css');
+parentDoc.body.appendChild(newStyleLink);
 
+
+// Apps stuff
+let apps = parentDoc.querySelector('body.ios, body.android');
+if (apps) {
+  let seriesTag = apps.querySelector('.tone--special.garnett--type-immersive .article__series');
+  let seriesTagText = apps.querySelector('.garnett--type-immersive .article__header-info .article__series-copy > a > span');
+  let headerBg = apps.querySelector('.garnett--type-immersive .article__header-info');
+  let seriesBagde = apps.querySelector('.garnett--type-immersive .article-badge');
+  let articleHeader = apps.querySelector('.article__header');
+  let articleBody = apps.querySelector('.article__body');
+
+  articleHeader.style = 'background-color: #fff';
+  articleBody.style = 'background-color: #fff';
+  seriesTag.style = 'background-color: #ffe500';
+  seriesTagText.style = 'background-color: #ffe500';
+  seriesTagText.style = 'color: #3f464a;font-weight: bold; font-size: 18px';
+  seriesBagde.style = 'width: 42px;margin-bottom: 6px;'
+  headerBg.style = 'background-color: #041F4A';
+
+} else {
+
+
+}
 
 // THIS IS A .find() Polyfill
 if (!Array.prototype.find) {
